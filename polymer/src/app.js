5bc486af3dbff4c5e0c3def931ee5018a2bc9058
abachar010515 <abdelhakim.bachar-ext@sgcib.com>
2015-12-22 10:30:09 +0100

    Implement polymer code

---
+++ polymer/src/app.js
@@ -0,0 +1,4 @@
+import Point from './point.js';
+
+var body = document.querySelector('body');
+body.textContent = 'Good point: ' + new Point(1, 23);
---
+++ polymer/webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: path.join(__dirname, 'src'), loader: 'babel-loader'}
+        ]
+    }
+};
