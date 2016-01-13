a104f80beeae418c8231dcdd5da4f7650f2234de
abachar010515 <abdelhakim.bachar-ext@sgcib.com>
2016-01-13 19:19:08 +0100

    Implement home page

--- aurelia/src/features/home/home.js
+++ aurelia/src/features/home/home.js
@@ -1,3 +1,17 @@
-export class Home {
+import {inject} from 'aurelia-framework';
+import {PostService} from '../../services/post';
 
+@inject(PostService)
+export class Home {
+    constructor(postService) {
+        this.filter = '';
+        this.posts = [];
+        this.postService = postService;
+    }
+
+    activate() {
+        this.postService
+            .findAll()
+            .then(posts => this.posts = posts);
+    }
 }
--- aurelia/src/services/post.js
+++ aurelia/src/services/post.js
@@ -1,3 +1,25 @@
-export class Post {
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
 
+@inject(HttpClient)
+export class PostService {
+    constructor(http) {
+        http.configure(config => {
+            config.withBaseUrl('/posts');
+        });
+
+        this.http = http;
+    }
+
+    findAll() {
+        return this.http
+            .fetch('/')
+            .then(response => response.json())
+    }
+
+    findByCode() {
+        return this.http
+            .fetch('/' + code)
+            .then(response => response.json())
+    }
 }
